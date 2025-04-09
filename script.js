73760a018c01b2699c786692f0d772aa84a2304d
Bhagath <bhagathnaresh2020@gmail.com>
2025-04-09 13:13:48 +0530

    data

--- script.js
+++ script.js
@@ -18,12 +18,3 @@ const handleButtonClick = e => {
 buttons.forEach(btn => {
   btn.addEventListener("click", handleButtonClick);
 });
-
-function myFunction() {
-  var x = document.getElementById("Demo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
